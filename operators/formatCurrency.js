c944e186fd11ac085f5510d5cbc9aacb8ab28e11
Quentin Vuilliot <quentin.vuilliot@kaes.fr>
2018-06-12 12:44:16 +0200

    formatCurrency

---
+++ operators/formatCurrency.js
@@ -0,0 +1,7 @@
+module.exports = (n, suffix = true) =>
+  (n && n.toLocaleString
+    ? n.toLocaleString("fr", {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+      })
+    : "?") + (suffix ? " €" : "")
