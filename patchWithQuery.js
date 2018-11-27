02a151b063b38cd57b4de85d0aef7056ddd60bb5
Sylvain Vuilliot <sylvain.vuilliot@kaes.fr>
2018-11-27 09:05:00 +0100

    patchWithQuery: allow operators returning error and result when patching

--- patchWithQuery.js
+++ patchWithQuery.js
@@ -4,7 +4,18 @@
 module.exports = store => {
   const patch = arg => {
     const p = Array.isArray(arg) ? store.query(arg) : arg
-    return store.patch(p)
+    if (p.err) {
+      // allow operator to return explicit error instead of a patch
+      return p
+    } else if (p.patch) {
+      // allow operator to return explicit result to client while patching
+      store.patch(p.patch)
+      return { res: p.res } // on ne retourne pas le patch qui est plutôt de l'implémentation interne (et peut être un peu lourd parfois)
+    } else {
+      // per default, we return the patch
+      //TODO: faut-il vraiment attendre le retour du promise (qui correspond à l'écriture dans le cas d'un persistableStore) ?
+      return store.patch(p)
+    }
   }
   return {
     query: store.query,
