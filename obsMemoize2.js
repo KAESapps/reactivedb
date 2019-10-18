f03c01867efc892f5f8892a4cfafbeede5adee1a
Sylvain Vuilliot <sylvain.vuilliot@kaes.fr>
2019-10-18 10:49:06 +0200

    fix obsMemoize2

--- obsMemoize2.js
+++ obsMemoize2.js
@@ -30,11 +30,19 @@ const set = (map, key1, key2, v) => {
 module.exports = (fn, name, hash1, hash2) => {
   const cache = new Map()
   return (arg1, arg2) => {
-    const key1 = isString(arg1) ? arg1 : hash1 ? hash1(arg1) : JSON.stringify
-    const key2 = isString(arg2) ? arg2 : hash2 ? hash1(arg2) : JSON.stringify
+    const key1 = isString(arg1)
+      ? arg1
+      : hash1
+      ? hash1(arg1)
+      : JSON.stringify(arg1)
+    const key2 = isString(arg2)
+      ? arg2
+      : hash2
+      ? hash1(arg2)
+      : JSON.stringify(arg2)
     let obs = get(cache, key1, key2)
     if (!obs) {
-      const debugKey = isDev && name ? `${name}/${key1}-${key2}` : undefined
+      const debugKey = isDev && name ? `${name}/${key1},${key2}` : undefined
       const compute = debugKey
         ? trackTime(fn(arg1, arg2), debugKey)
         : fn(arg1, arg2)
