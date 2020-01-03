67b6d83c35f54b40c18b8be0a174e840a830959a
Sylvain Vuilliot <sylvain.vuilliot@kaes.fr>
2020-01-03 17:38:29 +0100

    allow terminal operators to return a promise for a result

--- patchWithQuery.js
+++ patchWithQuery.js
@@ -3,40 +3,40 @@
 const log = require("./log").sub("patch")
 
 module.exports = store => {
-  const patch = (arg, metadata) => {
-    const p = Array.isArray(arg) ? store.query(arg) : arg
-    if (p.err) {
-      // allow operator to return explicit error instead of a patch
-      log.info("command refused", { cmd: arg, err: p.err, metadata })
-      return p
-    } else if (p.res) {
-      // allow operator to return explicit result to client while patching
-      log.debug("command accepted", {
-        cmd: arg,
-        res: p.res,
-        patch: p.patch,
-        metadata,
-      })
-      if (p.patch && Object.keys(p.patch).length) {
-        store.patch(p.patch, metadata)
-      } else {
-        log.warn("empty patch")
-      }
-      return { res: p.res } // on ne retourne pas le patch qui est plutôt de l'implémentation interne (et peut être un peu lourd parfois)
-    } else {
-      // per default, we return the patch
-      //TODO: faut-il vraiment attendre le retour du promise (qui correspond à l'écriture dans le cas d'un persistableStore) ?
-      if (p && Object.keys(p).length) {
-        log.debug("patch accepted", {
-          patch: p,
+  const patch = (arg, metadata) => Promise.resolve(Array.isArray(arg) ? store.query(arg) : arg).then(p => {
+      if (p.err) {
+        // allow operator to return explicit error instead of a patch
+        log.info("command refused", { cmd: arg, err: p.err, metadata })
+        return p
+      } else if (p.res) {
+        // allow operator to return explicit result to client while patching
+        log.debug("command accepted", {
+          cmd: arg,
+          res: p.res,
+          patch: p.patch,
           metadata,
         })
-        return store.patch(p, metadata)
+        if (p.patch && Object.keys(p.patch).length) {
+          store.patch(p.patch, metadata)
+        } else {
+          log.warn("empty patch")
+        }
+        return { res: p.res } // on ne retourne pas le patch qui est plutôt de l'implémentation interne (et peut être un peu lourd parfois)
       } else {
-        log.warn("empty patch")
+        // per default, we return the patch
+        //TODO: faut-il vraiment attendre le retour du promise (qui correspond à l'écriture dans le cas d'un persistableStore) ?
+        if (p && Object.keys(p).length) {
+          log.debug("patch accepted", {
+            patch: p,
+            metadata,
+          })
+          return store.patch(p, metadata)
+        } else {
+          log.warn("empty patch")
+        }
       }
-    }
-  }
+    })
+  
   return {
     query: store.query,
     patch,
