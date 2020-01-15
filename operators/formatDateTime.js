c88d05766049f92c43ddf5b8a7c433d30691d466
Quentin Vuilliot <quentin.vuilliot@kaes.fr>
2020-01-15 15:07:21 +0100

    date precision week

--- operators/formatDateTime.js
+++ operators/formatDateTime.js
@@ -1,3 +1,6 @@
+const format = require("date-fns/format").default || require("date-fns/format")
+const parseISO =
+  require("date-fns/parseISO").default || require("date-fns/parseISO")
 const isString = require("lodash/isString")
 const assign = require("lodash/assign")
 const datePrecisionAtLeast = require("./utils/datePrecision").atLeast
@@ -6,22 +9,27 @@ const datePrecisionAtLeast = require("./utils/datePrecision").atLeast
 // et le formate en date avec heure
 
 module.exports = (n, options = {}) => {
-  if (options.precision) {
-    const precisionAtLeast = datePrecisionAtLeast(options.precision)
-    options = assign(
-      {
-        year: "numeric",
-        month: precisionAtLeast("month") ? "numeric" : undefined,
-        day: precisionAtLeast("day") ? "numeric" : undefined,
-        hour: precisionAtLeast("hour") ? "numeric" : undefined,
-        minute: precisionAtLeast("minute") ? "numeric" : undefined,
-        second: precisionAtLeast("second") ? "numeric" : undefined,
-      },
-      options
-    )
-  }
-  if (!n || !isString(n)) return ''
+  if (!n || !isString(n)) return ""
 
+  if (options.precision) {
+    if (options.precision === "week") {
+      // traitement à part pour la précision semaine
+      return format(parseISO(n), "'S'II-yyyy")
+    } else {
+      const precisionAtLeast = datePrecisionAtLeast(options.precision)
+      options = assign(
+        {
+          year: "numeric",
+          month: precisionAtLeast("month") ? "numeric" : undefined,
+          day: precisionAtLeast("day") ? "numeric" : undefined,
+          hour: precisionAtLeast("hour") ? "numeric" : undefined,
+          minute: precisionAtLeast("minute") ? "numeric" : undefined,
+          second: precisionAtLeast("second") ? "numeric" : undefined,
+        },
+        options
+      )
+    }
+  }
   return new Date(n).toLocaleString(
     "fr",
     assign(
--- operators/utils/datePrecision.js
+++ operators/utils/datePrecision.js
@@ -4,9 +4,12 @@ module.exports = {
   precisionOrder,
   atLeast: precision => {
     const precisionIndex = precisionOrder.indexOf(precision)
-
-    return minPrecision => {
-      return precisionIndex >= precisionOrder.indexOf(minPrecision)
-    }
+    if (precision === "week")
+      return minPrecision => ["year", "week"].indexOf(minPrecision) !== -1
+    else
+      return minPrecision => {
+        const atLeastIdx = precisionOrder.indexOf(minPrecision)
+        return atLeastIdx !== -1 && precisionIndex >= atLeastIdx
+      }
   },
 }
