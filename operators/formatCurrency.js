0476cdefd53ac2b21935950a1ffee5880b894f38
Sylvain Vuilliot <sylvain.vuilliot@kaes.fr>
2020-07-24 17:26:10 +0200

    CURRENCY_SYMBOL personnalisable

--- operators/formatCurrency.js
+++ operators/formatCurrency.js
@@ -1,7 +1,8 @@
+const CURRENCY_SYMBOL = process.env.CURRENCY_SYMBOL || "€"
 module.exports = (n, suffix = true) =>
   (n && n.toLocaleString
     ? n.toLocaleString("fr", {
-      minimumFractionDigits: 2,
-      maximumFractionDigits: 2,
-    })
-    : "") + (suffix ? " €" : "")
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+      })
+    : "") + (suffix ? ` ${CURRENCY_SYMBOL}` : "")
