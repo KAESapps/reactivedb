343d58a850e0666813504c8652294e4610332520
Sylvain Vuilliot <sylvain.vuilliot@kaes.fr>
2021-02-27 07:15:53 +0100

    bindStore better error message

--- bindStoreOnConnection.js
+++ bindStoreOnConnection.js
@@ -1,24 +1,24 @@
-const isPromise = v => v && v.then
+const isPromise = (v) => v && v.then
 
 // expose a watchable store on a connection
 module.exports = (store, conn) => {
   const send = conn.postMessage
-  conn.onMessage(data => {
+  conn.onMessage((data) => {
     let callId
     try {
       callId = data.callId
       const res = store[data.method](data.arg)
       if (isPromise(res)) {
         res.then(
-          v => send({ callId, res: v }),
-          err => send({ callId, err: err.message })
+          (v) => send({ callId, res: v }),
+          (err) => send({ callId, err: err ? err.toString() : "error" })
         )
       } else {
         send({ callId, res })
       }
     } catch (err) {
       console.error("error responding to method call", err)
-      send({ callId, err: err.message })
+      send({ callId, err: err ? err.toString() : "error" })
       throw err
     }
   })
--- bindStoreOnServer.js
+++ bindStoreOnServer.js
@@ -1,13 +1,13 @@
 const get = require("lodash/get")
 const watchable = require("./watchableStore")
-const isPromise = v => v && v.then
+const isPromise = (v) => v && v.then
 const log = require("./log").sub("wsServer")
 
 // create a watchable store from store and expose it on a ws server
 module.exports = (store, wss) =>
   new Promise((resolve, reject) => {
     const logConnectedUsers = () => {
-      const names = Array.from(wss.clients).map(ws => ws.userName)
+      const names = Array.from(wss.clients).map((ws) => ws.userName)
       log.info("userConnectionsChanged", { count: names.length, names })
     }
     wss.on("connection", (ws, req) => {
@@ -17,14 +17,14 @@ module.exports = (store, wss) =>
       ws.userId = userId
       log.debug("new ws connection for user", userName)
       logConnectedUsers()
-      let send = data => {
-        ws.send(JSON.stringify(data), err => {
+      let send = (data) => {
+        ws.send(JSON.stringify(data), (err) => {
           if (err)
             log.warn("error sending data to client", { userId, userName }, err)
         })
       }
       const watchableStore = watchable(store, send)
-      ws.on("message", str => {
+      ws.on("message", (str) => {
         let data
         try {
           data = JSON.parse(str)
@@ -38,16 +38,16 @@ module.exports = (store, wss) =>
           res = watchableStore[data.method](data.arg, { user: userId })
         } catch (err) {
           log.error("error responding to method call", err)
-          send({ callId, err: err.message })
+          send({ callId, err: err ? err.toString() : "error" })
           throw err
         }
         if (isPromise(res)) {
           return res.then(
             //est-ce que le "return" est utile ?
-            v => send({ callId, res: v }),
-            err => {
+            (v) => send({ callId, res: v }),
+            (err) => {
               log.error("error responding to method call", err)
-              send({ callId, err: err.message })
+              send({ callId, err: err ? err.toString() : "error" })
               throw err
             }
           )
@@ -55,7 +55,7 @@ module.exports = (store, wss) =>
           send({ callId, res })
         }
       })
-      ws.on("close", code => {
+      ws.on("close", (code) => {
         const closedByUser = code == 1005
         log.debug("connection closed", { userName, closedByUser })
         logConnectedUsers()
