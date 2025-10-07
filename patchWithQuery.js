ac461ddbb4cb653267a8b6361f357741578f062b
Sylvain Vuilliot <sylvain.vuilliot@kaes.fr>
2025-10-07 09:35:28 +0200

    patchWithQuery : descend le niveau de gravité d'un promise de patch rejeté de error à warn (car ce n'est pas si grave)

--- patchWithQuery.js
+++ patchWithQuery.js
@@ -1,5 +1,10 @@
-// permet d'augmenter un store ayant une méthode "patch" simple avec une
-// méthode qui permet de générer un patch à partir d'une query (sous forme d'un array)
+/*
+ permet d'augmenter un store ayant une méthode "patch" simple avec une
+ méthode qui permet de générer un patch à partir d'une query (sous forme d'un array)
+ la convention est qu'un opérateur peut retourner soit un patch directement, soit une réponse {res, err, patch}
+ et cela soit de façon synchrone ou asynchrone. En asynchrone, le promise doit renvoyer une réponse comme en synchrone mais peut aussi rejeter une erreur, dans ce cas, elle est traitée comme un resolve({err})
+ En synchrone
+*/
 const log = require("./log").sub("patch")
 
 module.exports = (store) => {
@@ -49,12 +54,14 @@ module.exports = (store) => {
 
   const patch = (arg, metadata) => {
     const ret = Array.isArray(arg) ? store.query(arg) : arg
-    // TODO: on ne retourne un promise que si la création du patch est asynchrone car sinon ça casse la synchro mobile.... pas terrible !!!
+    // on ne retourne un promise que si la création du patch est asynchrone car sinon ça casse la synchro mobile.... pas terrible !!!
+    // on ne catche pas les queries synchrone car kobs n'est pas prévu pour continuer si un opérateur plante... donc il faut redémarrer
+    // mais on catche les opérateurs asynchrones car leurs résultats ne sont pas utilisés en réactif (ce sont plutôt des effets de bord ou des patchs décalés)
     return ret && ret.then
       ? ret
           .then((p) => handleRet(p, arg, metadata))
           .catch((err) => {
-            log.error("patching error", { arg, metadata }, err)
+            log.warn("patching error", { arg, metadata }, err)
             return { err: err + "" }
           })
       : handleRet(ret, arg, metadata)
