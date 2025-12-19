261155078ba500420a008d2e0c187f1a6a4b9ebc
Quentin Vuilliot <quentin.vuilliot@kaes.fr>
2025-12-19 18:06:32 +0100

    Fix format ISO week

--- operators/isoDateTimeToPrecision.js
+++ operators/isoDateTimeToPrecision.js
@@ -2,15 +2,14 @@
 Accepte en argument une instance de date ou une chaine ISO (en fait un argument de new Date)
 */
 const formatISO = require("date-fns/formatISO")
-const getISOWeekYear = require("date-fns/getISOWeekYear")
-const getISOWeek = require("date-fns/getISOWeek")
+const format = require("date-fns/format")
 
 module.exports = (precision) => (arg) => {
   if (!arg) return null
   const date = new Date(arg)
   //TODO: if not isValid date return null
   if (precision === "date") return formatISO(date, { representation: "date" })
-  if (precision === "week") return `${getISOWeekYear(date)}W${getISOWeek(date)}`
+  if (precision === "week") return format(date, "RRRR'W'II")
   if (precision === "month")
     return formatISO(date, { representation: "date" }).slice(0, 7)
   if (precision === "year")
